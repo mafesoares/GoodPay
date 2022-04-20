627554a020573571698c13bb034abae800423bc1
mafesoares <mfsoares00@gmail.com>
2022-04-20 14:03:24 -0300

    Banner com textos atualizados e Carrossel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-var janelaLogin = new bootstrap.Modal(document.getElementById("loginNegado"));
+var janelaLogin = new bootstrap.Modal(document.getElementById('loginNegado'));
+var janelaPromocao = new bootstrap.Modal(document.getElementById('promo1'));
 
-function rotaCadastro() {
-    window.location.href = "../registro.html";
+function rotaCadastro(){
+    window.location.href ='../registro.html';
 }
-function rotaRecuperar() {
-    window.location.href = "../recuperar.html";
+
+function rotaRecuperar(){
+    window.location.href ='../recuperar.html';
 }
-function rotaLogin() {
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
-    if (email === "mfsoares00@gmail.com" && senha === "123456") {
-        window.location.href = "../index.html";
-    } else {
+function rotaLogin(){
+    let email = document.getElementById('email').value;
+    let senha = document.getElementById('senha').value;
+    if(email === 'mfsoares00@gmail.com' && senha === '123456'){
+        window.location.href ='../index.html';
+    }else{
         janelaLogin.show();
     }
+}
+
+function alertaPromo(){
+    let titulo = "Promoção 1 [Quebra da Banca]";
+    let corpo = "<h6>Super legal</h6> <p><img src='./img/4.png' width='160' height='160'></p> ";
+       
+    janelaPromocao.show();
+
+    document.getElementById("titulo").innerHTML = titulo;
+    document.getElementById("corpo").innerHTML = corpo;
 }
